afe979f0311e18e0fd604c872e1292e46c2e05f5
Mostafa Gamal <m0gz@yahoo.com>
2021-06-18 21:17:45 +0200

    bugs fixed in vir pen

--- canvas.py
+++ canvas.py
@@ -13,95 +13,88 @@ def canvas():
 
     kernel = np.ones((5,5),np.uint8)
 
-    # Initializing the canvas on which we will draw upon
-    canvas = None
+    # Initializing the plane on which we will draw upon
+    plane = None
 
     # Initilize x1,y1 points
     x1,y1=0,0
 
     # Threshold for noise
-    noiseth = 800
+    noiseThreshold = 800
 
     while(1):
         _, frame = cap.read()
         frame = cv2.flip( frame, 1 )
         
-        # Initialize the canvas as a black image of the same size as the frame.
-        if canvas is None:
+        # Initialize the plane as a black image of the same size as the frame.
+        if plane is None:
 
-            #canvas = cv2.resize(cv2.imread('c://Users//osama//Desktop//image//pen.jpg',1), (1280, 720))
+            #plane = cv2.resize(cv2.imread('c://Users//osama//Desktop//image//pen.jpg',1), (1280, 720))
 
-            canvas = np.zeros_like(frame)
-            canvas[np.where((canvas==[0,0,0]).all(axis=2))] = [0,128,128]
+            plane = np.zeros_like(frame)
+            plane[np.where((plane==[0,0,0]).all(axis=2))] = [0,128,128]
 
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
-        # If you're reading from memory then load the upper and lower ranges 
-        # from there
+
+        #In case of loading the upper and lower ranges from memory
         if load:
-            lower_range = penval[0]
-            upper_range = penval[1]
+            lowerRange = penval[0]
+            upperRange = penval[1]
                 
-        # Otherwise define your own custom values for upper and lower range.
+        #Otherwise define your own values
         else:             
-            lower_range  = np.array([26,80,147])
-            upper_range = np.array([81,255,255])
+            lowerRange  = np.array([26,80,147])
+            upperRange = np.array([81,255,255])
         
-        mask = cv2.inRange(hsv, lower_range, upper_range)
+        mask = cv2.inRange(hsv, lowerRange, upperRange)
         
-        # Perform morphological operations to get rid of the noise
+        #get rid of the noise by performing morphological operations
         mask = cv2.erode(mask,kernel,iterations = 1)
         mask = cv2.dilate(mask,kernel,iterations = 2)
         
         # Find Contours
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        # Make sure there is a contour present and also its size is bigger than 
-        # the noise threshold.
+        #contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        #check that contour is bigger than noise threshold
         if contours and cv2.contourArea(max(contours, 
-                                    key = cv2.contourArea)) > noiseth:
+                                    key = cv2.contourArea)) > noiseThreshold:
                     
-            c = max(contours, key = cv2.contourArea)    
-            x2,y2,w,h = cv2.boundingRect(c)
+            bound = max(contours, key = cv2.contourArea)    
+            x2,y2,w,h = cv2.boundingRect(bound)
             
-            # If there were no previous points then save the detected x2,y2 
-            # coordinates as x1,y1. 
-            # This is true when we writing for the first time or when writing 
-            # again when the pen had disappeared from view.
+            # If no earlier points were found, save the detected x2,y2 coordinates as  x1,y1 
+            # # This happens when writing for the first time or when writing again when the pen had disappeared from view.
             if x1 == 0 and y1 == 0:
                 x1,y1= x2,y2
                 
             else:
-                # Draw the line on the canvas
-                canvas = cv2.line(canvas, (x1,y1),(x2,y2), [128,128,0], 4)
+        
+                plane = cv2.line(plane, (x1,y1),(x2,y2), [128,128,0], 4)
             
-            # After the line is drawn the new points become the previous points.
+            #the new points become the previous points.
             x1,y1= x2,y2
 
         else:
-            # If there were no contours detected then make x1,y1 = 0
+
+            #if no contours decteced then x1,y1=0
             x1,y1 =0,0
         
-        # Merge the canvas and the frame.
-        #frame = cv2.add(frame,canvas)
-        
-        # Optionally stack both frames and show it.
-        #stacked = np.hstack((canvas,frame))
-        cv2.imshow('Trackbars',cv2.resize(canvas,None,fx=0.6,fy=0.6))
+        cv2.imshow('Trackbars',cv2.resize(plane,None,fx=0.6,fy=0.6))
 
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
-            # If the user presses `s` then print this array.
-        if k == ord('s'):
-            print(type(canvas))
-            cv2.imwrite('./save//EI.jpg',canvas)
+            # press 'p' to print this array
+        if k == ord('p'):
+            print(type(plane))
+            cv2.imwrite('./save//EI.jpg',plane)
             break
-        # When c is pressed clear the canvas
+        # When c is pressed clear the plane
         if k == ord('c'):
-            canvas = None
+            plane = None
 
     cv2.destroyAllWindows()
     cap.release()
--- virtual_pen.py
+++ virtual_pen.py
@@ -6,29 +6,30 @@ def pen():
     load = True
     if load:
         penval = np.load('setupObject.npy')
-
+        
+    # Capture CAM object
     cap = cv2.VideoCapture(0)
 
-    # Load these 2 images and resize them to the same size.
-    pen_img = cv2.resize(cv2.imread('./pen.jpg',1), (50, 50))
-    eraser_img = cv2.resize(cv2.imread('./eraser.jpg',1), (50, 50))
+    #Read pen and eraser image and resize them
+    penImage = cv2.resize(cv2.imread('./pen.jpg',1), (50, 50))
+    eraserImage = cv2.resize(cv2.imread('./eraser.jpg',1), (50, 50))
 
     kernel = np.ones((5,5),np.uint8)
 
-    # Making window size adjustable
+    # make window adjustable
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
-    # This is the canvas on which we will draw upon
+    #This will be the canvas (board like a paper) on which we will draw
     canvas = None
 
     # Create a background subtractor Object
     backgroundobject = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
 
-    # This threshold determines the amount of disruption in the background.
-    background_threshold = 600
+    #Background threshold responsiable for the amount of disruption in the background
+    backgroundThreshold = 600
 
     # A variable which tells you if you're using a pen or an eraser.
-    switch = 'Pen'
+    currentObject = 'Pen'
 
     # With this variable we will monitor the time between previous switch.
     last_switch = time.time()
@@ -37,10 +38,10 @@ def pen():
     x1,y1=0,0
 
     # Threshold for noise
-    noiseth = 800
+    noiseThreshold = 800
 
-    # Threshold for wiper, the size of the contour must be bigger than this for # us to clear the canvas
-    wiper_thresh = 40000
+    #Threshold for wiper , to clear the canvas the countour must be bigger than this
+    wiperThreshold = 40000
 
     # A variable which tells when to clear canvas
     clear = False
@@ -49,49 +50,52 @@ def pen():
         _, frame = cap.read()
         frame = cv2.flip( frame, 1 )
         
-        # Initilize the canvas as a black image
+        
+        #Init canvas as a blck image
         if canvas is None:
             canvas = np.zeros_like(frame)
             
-        # Take the top left of the frame and apply the background subtractor
-        # there    
+        #applay background subtractor on the top left of the frame
         top_left = frame[0: 50, 0: 50]
         fgmask = backgroundobject.apply(top_left)
         
-        # Note the number of pixels that are white, this is the level of 
-        # disruption.
-        switch_thresh = np.sum(fgmask==255)
+        switchThreshold = np.sum(fgmask==255)
         
         # If the disruption is greater than background threshold and there has 
-        # been some time after the previous switch then you. can change the 
-        # object type.
-        if switch_thresh>background_threshold and (time.time()-last_switch) > 1:
+        # been some time after the previous switch then you can change the 
+        
+        if switchThreshold > backgroundThreshold and (time.time()-last_switch) > 1:
 
             # Save the time of the switch. 
+            # to be used in switching between pen and eraser
             last_switch = time.time()
             
-            if switch == 'Pen':
-                switch = 'Eraser'
+            if currentObject == 'Pen':
+                currentObject = 'Eraser'
             else:
-                switch = 'Pen'
+                currentObject = 'Pen'
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
-        # If you're reading from memory then load the upper and lower ranges 
-        # from there
+        
+        #loading setup object from memory
         if load:
-                lower_range = penval[0]
-                upper_range = penval[1]
+                lowerRange = penval[0]
+                upperRange = penval[1]
                 
-        # Otherwise define your own custom values for upper and lower range.
+ 
+        #if there is no penvel object (previous setup)
+        #there is a lower and upper range defination below in case of no setup 
+        #(you can easily change it as needed)
         else:             
-            lower_range  = np.array([26,80,147])
-            upper_range = np.array([81,255,255])
+            lowerRange  = np.array([26,80,147])
+            upperRange = np.array([81,255,255])
         
-        mask = cv2.inRange(hsv, lower_range, upper_range)
+        mask = cv2.inRange(hsv, lowerRange, upperRange)
         
-        # Perform morphological operations to get rid of the noise
+       
+        #get rid of nnoise by applaying erosion and dialation operations (morphological operations)
         mask = cv2.erode(mask,kernel,iterations = 1)
         mask = cv2.dilate(mask,kernel,iterations = 2)
         
@@ -99,10 +103,9 @@ def pen():
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, 
         cv2.CHAIN_APPROX_SIMPLE)
         
-        # Make sure there is a contour present and also it size is bigger than 
-        # noise threshold.
+        #make sure that there is a contour and bigger than noiseThreshold
         if contours and cv2.contourArea(max(contours,
-                                        key = cv2.contourArea)) > noiseth:
+                                        key = cv2.contourArea)) > noiseThreshold:
                     
             c = max(contours, key = cv2.contourArea)    
             x2,y2,w,h = cv2.boundingRect(c)
@@ -116,7 +119,7 @@ def pen():
                 x1,y1= x2,y2
                 
             else:
-                if switch == 'Pen':
+                if currentObject == 'Pen':
                     # Draw the line on the canvas
                     canvas = cv2.line(canvas, (x1,y1),
                     (x2,y2), [255,0,0], 5)
@@ -125,24 +128,22 @@ def pen():
                     cv2.circle(canvas, (x2, y2), 20,
                     (0,0,0), -1)
                 
-                
-            
-            # After the line is drawn the new points become the previous points.
+                        
+            #swaping between points after the line is drawn
             x1,y1= x2,y2
             
-            # Now if the area is greater than the wiper threshold then set the 
-            # clear variable to True
-            if area > wiper_thresh:
+            #if area greater than wiper then set clear to true
+            if area > wiperThreshold:
                 cv2.putText(canvas,'Clearing Canvas',(0,200), 
                 cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 1, cv2.LINE_AA)
                 clear = True 
 
         else:
-            # If there were no contours detected then make x1,y1 = 0
+            # If there were no contours detected then set x1,y1 = 0
             x1,y1 =0,0
         
     
-        # Now this piece of code is just for smooth drawing. (Optional)
+        #for smoth drawing (increase quality of drwaing )
         _ , mask = cv2.threshold(cv2.cvtColor (canvas, cv2.COLOR_BGR2GRAY), 20, 
         255, cv2.THRESH_BINARY)
         foreground = cv2.bitwise_and(canvas, canvas, mask = mask)
@@ -151,11 +152,12 @@ def pen():
         frame = cv2.add(foreground,background)
 
         # Switch the images depending upon what we're using, pen or eraser.
-        if switch != 'Pen':
+        #switch the upper left image denpnding on pen or eraser is used  
+        if currentObject != 'Pen':
             cv2.circle(frame, (x1, y1), 20, (255,255,255), -1)
-            frame[0: 50, 0: 50] = eraser_img
+            frame[0: 50, 0: 50] = eraserImage
         else:
-            frame[0: 50, 0: 50] = pen_img
+            frame[0: 50, 0: 50] = penImage
 
         cv2.imshow('image',frame)
 
@@ -163,12 +165,12 @@ def pen():
         if k == 27:
             break
         
-        # Clear the canvas after 1 second, if the clear variable is true
+        #if clear variable is true then clear canvas after 1 sec
         if clear == True: 
             time.sleep(1)
             canvas = None
             
-            # And then set clear to false
+            
             clear = False
             
     cv2.destroyAllWindows()
