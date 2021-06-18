5a738e858105a1ea4791adaea9671160fbf4dda4
Swilam20 <38882447+Swilam20@users.noreply.github.com>
2021-06-18 20:46:12 +0200

    Update print_message.py

--- print_message.py
+++ print_message.py
@@ -1,27 +1,18 @@
-#================================================#
-# Function to save text-string inside a txt file #
-#    and save this file to a specific folder     #
-#================================================#
-
-# Packages
+#function to string inside text file in specific folder 
 import os, os.path
-from datetime import datetime
-
-# Current date and time as in string format
-date_time = datetime.now().strftime("%d-%m-%y_%H:%M")
-# %d-%m-%y_%H:%M : day-month-year_hours:minutes
 
 # Directory (folder) of messages
-dir_name = 'messages'
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name) # Create folder if not exists
+dirName = 'messages'
+#if path dosen't exists then create new one
+if not os.path.exists(dirName):
+    os.mkdir(dirName) 
 
-# Count the number of the saved messages
-number_of_messages = len([messages for messages in os.listdir(dir_name) if os.path.isfile(os.path.join(dir_name, messages))])
+#calcualte the number of saved messages
+number_of_messages = len([messages for messages in os.listdir(dirName) if os.path.isfile(os.path.join(dirName, messages))])
 
-# Name of new message : "message(number)_day-month-year_hour:minutes.txt"
-new_message = 'message' + str(number_of_messages+1) + '_' + date_time + '.txt'
+# Name of new message : "message+number.txt"
+new_message = 'message' + str(number_of_messages+1)+'.txt'
 
 def export_message(text):
-    with open(dir_name+'/'+new_message, 'w') as myfile:
+    with open(dirName+'/'+new_message, 'w') as myfile:
         myfile.write(text)
